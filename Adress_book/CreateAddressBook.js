c0f9df7d013e9c1ddb9692cc79642446f810010f
Harsh Singh <kmrharsh50@gmail.com>
2022-01-28 11:35:43 +0530

    [Harsh]Added:Find from name and delete contacts AddressBook_JS feature implemented

--- Adress_book/CreateAddressBook.js
+++ Adress_book/CreateAddressBook.js
@@ -188,11 +188,11 @@ try {
   let check = ContactsArray.findIndex(
     (contact_book) => contact_book.firstName == "Richa"
   );
-  //updating the contact detail
-  ContactsArray[check].phoneNumber = 7083280401;
-
-  //displaying contacts after being updated
-  console.log("--------After Updating pnoneNumber--------------");
+  //Removing an element from an array at an index
+  ContactsArray.splice(check, 1);
+  console.log(
+    "----------------Contacts after Being Deleted from the array------------------------"
+  );
   ContactsArray.forEach((contact_book) => console.log(contact_book.toString()));
 } catch (e) {
   console.log(e);
